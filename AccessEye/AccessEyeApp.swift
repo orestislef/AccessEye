899edab2dd0d68eedff547cb3baf618a74fb12a6
Orestis MacBook Air Work <orestis_94@hotmail.com>
2026-06-03 10:12:09 +0300

    Initial Commit

---
+++ AccessEye/AccessEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessEyeApp.swift
+//  AccessEye
+//
+//  Created by IqTaxi on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AccessEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
